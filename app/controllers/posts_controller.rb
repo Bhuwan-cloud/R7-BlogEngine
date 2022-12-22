0234870a2c452fe9fd9678ad223f339245e995ea
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2022-12-22 09:30:49 +0545

    views counter added to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,11 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    views = @post.views + 1
+    @post.views = views
+    @post.save
+    #  OR
+    # @post.update(views: @post.views + 1)
   end
 
   # GET /posts/new
