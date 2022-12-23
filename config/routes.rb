d2a1cb74f527bcefe92f314529c5392965ae749a
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2022-12-23 10:11:17 +0545

    views counter added to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "users/profile"
   resources :posts
   root("pages#home")
   get("about", to: "pages#about")
   devise_for(:users, controllers: { sessions: "users/sessions", registrations: "users/registrations" })
+  get("u/:id", :to => "users#profile", :as => "user")
 end
